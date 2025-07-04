41475ac09e7e1b418d1b41cd7edeb1e26b9e9c75
tuychiyevkm <tmkomron@gmail.com>
2025-07-04 07:34:57 +0500

    bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,14 @@
+import json
+
+with open("students.json") as f:
+    students = json.load(f)
+
+students_s = sorted(students, key=lambda x: x["score"], reverse=True)
+
+for i, student in enumerate(students_s, start=1):
+    student["rank"] = i
+
+with open("rating.json", "w") as f1:
+    json.dump(students_s, f1, indent=2)
+
 
